875b3fb57c17f91b0698f8ea987b043fecce4305
s23245 <s23245@pjwstk.edu.pl>
2025-01-12 15:33:08 +0100

    - Amplify settings fix #4

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client';
 import App from './App';
 import './index.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import Amplify from 'aws-amplify';
+import {Amplify} from 'aws-amplify';
 import awsExports from './aws-exports';
 
 

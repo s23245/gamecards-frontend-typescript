e821121521ba8f6cfab7018117b549e7e4b8f4e3
s23245 <s23245@pjwstk.edu.pl>
2025-01-14 17:51:13 +0100

    - Amplify settings fix #7

--- src/services/HelloService.ts
+++ src/services/HelloService.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const BASE_URL = 'https://gamecardsbackendjava-env.eba-g228jg6f.us-east-1.elasticbeanstalk.com';
+const BASE_URL = 'http://gamecardsbackendjava-env.eba-g228jg6f.us-east-1.elasticbeanstalk.com';
 
 export async function getHelloMessage() {
     const response = await axios.get(`${BASE_URL}/api/hello`);
